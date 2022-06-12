8643a8aafd81dbec9acc461eceff45b57419a86f
Lily Wang <31115101+lilyminium@users.noreply.github.com>
2022-06-12 15:52:32 +1000

    added fixture and test

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/tests/test_{{cookiecutter.repo_name}}.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/tests/test_{{cookiecutter.repo_name}}.py
@@ -7,6 +7,13 @@ import {{cookiecutter.repo_name}}
 import pytest
 import sys
 
+
 def test_{{cookiecutter.repo_name}}_imported():
     """Sample test, will always pass so long as import statement worked"""
     assert "{{cookiecutter.repo_name}}" in sys.modules
+
+
+def test_mdanalysis_logo_length(mdanalysis_logo_text):
+    """Example test using a fixture defined in conftest.py"""
+    logo_lines = mdanalysis_logo_text.split("\n")
+    assert len(logo_lines) == 46, "Logo file does not have 46 lines!"
