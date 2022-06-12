8702da797ab67dd86c9ea4f5dc42e7d375429918
Lily Wang <31115101+lilyminium@users.noreply.github.com>
2022-06-12 15:20:55 +1000

    Update hook scripts to use main branch (#5)
    * rewrite hook scripts

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -7,24 +7,23 @@ If any error is raised, the cookie cutter creation fails and crashes
 import re
 import sys
 
-MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
-EMAIL_REGEX = r'[^@]+@[^@]+\.[^@]+'
 
-repo_name = '{{ cookiecutter.repo_name }}'
-module_name = '{{ cookiecutter.first_module_name }}'
-
-author_email = '{{ cookiecutter.author_email }}'
-
-for key in [repo_name, module_name]:
-    if not re.match(MODULE_REGEX, key):
-        print(key, re.match(MODULE_REGEX, key))
-        print('ERROR: "{}" is not a valid Python module name!'.format(key))
+def validate(regex: str, value: str, field: str):
+    if not re.match(regex, value):
+        print('ERROR: "{}" is not a valid {}!'.format(value, field))
 
         # exits with status 1 to indicate failure
         sys.exit(1)
 
-if not re.match(EMAIL_REGEX, author_email):
-    print('ERROR: "{}" is not a valid email address!'.format(author_email))
 
-    # exits with status 1 to indicate failure
-    sys.exit(1)
+if __name__ == "__main__":
+    REGEX_EMAIL = r'^[^@]+@[^@]+\.[^@]+$'
+    REGEX_MODULE = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
+
+    validate(REGEX_EMAIL, "{{ cookiecutter.author_email }}", "email")
+    validate(REGEX_MODULE, "{{ cookiecutter.repo_name }}", "repo name")
+    validate(
+        REGEX_MODULE,
+        "{{ cookiecutter.first_module_name }}",
+        "module name"
+    )
