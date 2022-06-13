34bed1b22c8943c6ff4ab3277c95a1c19941ad32
Lily Wang <31115101+lilyminium@users.noreply.github.com>
2022-06-13 10:21:31 +1000

    trial injected variables

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -2,12 +2,42 @@
 Pre Cookie Generation script(s)
 
 If any error is raised, the cookie cutter creation fails and crashes
+
+
+
+=== VARIABLE INJECTION ===
+This next section uses the fact that the script is rendered with Jinja
+prior to execution to inject values into the template.
+
+It checks to see if the `github_host_account` is the default value,
+i.e. the instructional help text. If so, it replaces the value with
+`github_username`.
+
+Then it adds a `github_url` variable for easier use with CI, etc.
+
+{{ cookiecutter.update({"github_host_account": cookiecutter.github_host_account | trim }) }}
+
+{% if (not cookiecutter.github_host_account or "organization account" in cookiecutter.github_host_account) %}
+    {{ cookiecutter.update({ "github_host_account": cookiecutter.github_username }) }}
+{% else %}
+    {{ cookiecutter.update({ "github_host_account": cookiecutter.github_host_account }) }}
+{% endif %}
+
+{{ cookiecutter.update({"github_url": [cookiecutter.github_host_account, cookiecutter.repo_name]|join("/") }) }}
+
 """
 
 import re
 import sys
 
+<<<<<<< HEAD
 
+=======
+REGEX_EMAIL = r'^[^@]+@[^@]+\.[^@]+$'
+REGEX_MODULE = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
+
+
+>>>>>>> trial injected variables
 def validate(regex: str, value: str, field: str):
     if not re.match(regex, value):
         print('ERROR: "{}" is not a valid {}!'.format(value, field))
@@ -17,11 +47,21 @@ def validate(regex: str, value: str, field: str):
 
 
 if __name__ == "__main__":
+<<<<<<< HEAD
     REGEX_EMAIL = r'^[^@]+@[^@]+\.[^@]+$'
     REGEX_MODULE = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
 
     validate(REGEX_EMAIL, "{{ cookiecutter.author_email }}", "email")
     validate(REGEX_MODULE, "{{ cookiecutter.repo_name }}", "repo name")
+=======
+
+    validate(REGEX_EMAIL, "{{ cookiecutter.author_email }}", "email")
+    validate(REGEX_MODULE, "{{ cookiecutter.repo_name }}", "repo name")
+    validate(REGEX_MODULE,
+             "{{ cookiecutter.github_username }}", "GitHub username")
+    validate(REGEX_MODULE,
+             "{{ cookiecutter.github_host_account }}", "GitHub account")
+>>>>>>> trial injected variables
     validate(
         REGEX_MODULE,
         "{{ cookiecutter.first_module_name }}",
