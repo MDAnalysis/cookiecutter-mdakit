04240a39d91e00fda1a67fd2c01d65910cbb323e
Lily Wang <31115101+lilyminium@users.noreply.github.com>
2022-07-26 21:45:03 +1000

    Get started on analysis (#19)
    Co-authored-by: Irfan Alibay <IAlibay@users.noreply.github.com>
    
    * Add analysis template
    * Add tests for cookiecutter

---
+++ tests/test_options.py
@@ -0,0 +1,28 @@
+import pytest
+from cookiecutter.main import cookiecutter
+
+from .utils import CookiecutterMDAKit
+
+
+class TestAnalysis:
+
+    @pytest.mark.parametrize("analysis_name", ["", "Enter"])
+    def test_no_analysis(self, tmpdir, analysis_name):
+        with tmpdir.as_cwd():
+            kit = CookiecutterMDAKit(template_analysis_class=analysis_name)
+            kit.run()
+
+            assert not kit.cookie_package_path_exists("analysis")
+            assert not kit.cookie_package_path_exists("tests/analysis")
+
+    def test_analysis(self, tmpdir):
+        with tmpdir.as_cwd():
+            kit = CookiecutterMDAKit(template_analysis_class="MyAnalysisClass")
+            kit.run()
+
+            for clsname, file in (
+                ("MyAnalysisClass", "analysis/myanalysisclass.py"),
+                ("TestMyAnalysisClass", "tests/analysis/test_myanalysisclass.py"),
+            ):
+                assert kit.cookie_package_path_exists(file)
+                assert clsname in kit.get_classes_from_package_file(file)
---
+++ tests/utils.py
@@ -0,0 +1,77 @@
+import ast
+import enum
+from dataclasses import dataclass, asdict
+import pathlib
+from typing import Literal, List
+
+from cookiecutter.main import cookiecutter
+
+COOKIECUTTER_PATH = pathlib.Path(__file__).parent.parent.resolve()
+
+
+def get_classes_from_file(path: str) -> List:
+    path = pathlib.Path(path)
+    with path.open() as f:
+        tree = ast.parse(f.read(), filename=str(path))
+
+    classes: List[str] = []
+    for node in ast.iter_child_nodes(tree):
+        if isinstance(node, ast.ClassDef):
+            classes.append(node.name)
+    return classes
+
+
+IncludeReadTheDocs = Literal["y", "n"]
+
+class DependencyType(enum.Enum):
+    CONDAFORGE = "Prefer conda-forge over the default anaconda channel with pip fallback"
+    ANACONDA = "Prefer default anaconda channel with pip fallback"
+    PIP = "Dependencies from pip only (no conda)"
+
+
+@dataclass
+class CookiecutterMDAKit:
+    project_name: str = "TestMDAKitProjectName"
+    repo_name: str = "test-mda-kit"
+    package_name: str = "test_mdakit_package"
+    github_username: str = "test-user-account"
+    github_host_account: str = "test-github-host-account"
+    author_name: str = "Test User name"
+    author_email: str = "test_email@test.com"
+    description: str = "Test MDAKit Project description"
+    dependency_source: DependencyType = DependencyType.CONDAFORGE
+    include_ReadTheDocs: IncludeReadTheDocs = "y"
+    template_analysis_class: str = "MyAnalysisClass"
+
+    @property
+    def cookie_directory(self) -> pathlib.Path:
+        return pathlib.Path(self.repo_name)
+
+    @property
+    def package_directory(self) -> pathlib.Path:
+        return self.cookie_directory / self.package_name
+
+    def run(self):
+        context = {}
+        for k, v in asdict(self).items():
+            if isinstance(v, enum.Enum):
+                v = v.value
+            context[k] = v
+
+        return cookiecutter(
+            str(COOKIECUTTER_PATH),
+            no_input=True,
+            extra_context=context,
+        )
+
+    def cookie_path_exists(self, path: str) -> bool:
+        path = self.cookie_directory / path
+        return path.exists()
+
+    def cookie_package_path_exists(self, path: str) -> bool:
+        path = self.package_directory / path
+        return path.exists()
+
+    def get_classes_from_package_file(self, path: str) -> List[str]:
+        path = self.package_directory / path
+        return get_classes_from_file(path)
