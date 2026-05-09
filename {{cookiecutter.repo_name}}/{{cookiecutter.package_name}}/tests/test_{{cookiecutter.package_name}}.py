c7fc43cef34714e2d169bd2cd9f579d1ebf3c1d2
ojeda-e <estefania.b.ojeda@gmail.com>
2026-05-09 17:45:01 -0600

    Enhance: Add dummy function w/ parametrized tests

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/tests/test_{{cookiecutter.package_name}}.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/tests/test_{{cookiecutter.package_name}}.py
@@ -6,6 +6,7 @@ Unit and regression test for the {{cookiecutter.package_name}} package.
 import sys
 import pytest
 import {{cookiecutter.package_name}}
+from {{cookiecutter.package_name}}.example_api import example_function
 
 
 def test_{{cookiecutter.package_name}}_imported():
@@ -17,3 +18,16 @@ def test_mdanalysis_logo_length(mdanalysis_logo_text):
     """Example test using a fixture defined in conftest.py"""
     logo_lines = mdanalysis_logo_text.split("\n")
     assert len(logo_lines) == 46, "Logo file does not have 46 lines!"
+
+@pytest.mark.parametrize(
+    "value, expected",
+    [
+        (0, 0),
+        (1, 2),
+        (-5, -25),
+        (1.5, 3.0),
+        (-2.0, -4.0)
+    ],
+)
+def test_double_value(value, expected):
+    assert example_function(value) == expected
